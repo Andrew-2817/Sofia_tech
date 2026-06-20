ca57f75217ccff334beb8bc5555510eb7766c51d
Elisey <luisdimf@yandex.ru>
2026-06-20 15:40:45 +0300

    Обновление админ-панели: добавлены разделы для всех брендов, поиск во всех разделах

--- Backend/app/models/__init__.py
+++ Backend/app/models/__init__.py
@@ -1,13 +1,20 @@
-﻿from .user import User
-from .category import Category
-from .brand import Brand
-from .order import Order
 from .product import Product
+from .category import Category
+from .order import Order
+from .user import User
+from .brand import Brand
 
-__all__ = [
-    "User",
-    "Category",
-    "Brand",
-    "Order",
-    "Product",
-]
+# Импорты для товаров каждого бренда
+from .product_ilve import IlveProduct
+from .product_brandt import BrandtProduct
+from .product_bonkrasher import BonkrasherProduct
+from .product_dedietrich import DedietrichProduct
+from .product_falmec import FalmecProduct
+from .product_graude import GraudeProduct
+from .product_homeier import HomeierProduct
+from .product_kuppersbusch import KuppersbuschProduct
+from .product_liebherr import LiebherrProduct
+from .product_nivona import NivonaProduct
+from .product_schulthess import SchulthessProduct
+from .product_teka import TekaProduct
+from .product_elica import ElicaProduct
